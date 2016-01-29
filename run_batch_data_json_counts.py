694a01f3021c90443d8dbd7e03a91c34b8f81ba1
David Portnoy <david@portnoy.us>
2016-01-29 12:52:56 -0600

    Fixed batch run using jinja2 templates without Flask

--- run_batch_data_json_counts.py
+++ run_batch_data_json_counts.py
@@ -1,49 +1,22 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+import jinja2
 import os
 import data_json_counts  # Code to run data.json dataset counts
 
- 
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-app = Flask(__name__, template_folder=tmpl_dir)
+RENDERED_PATH = os.path.abspath('generated/')+'/'
+TEMPLATE_PATH = os.path.abspath('templates/')+'/'
+TEMPLATE_NAME = 'dataset_count_by_source.html'
 
-
-@app.route('/shutdown', methods=['POST'])
-def shutdown_server():
-    print( 'Server shutting down...')
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
-
-
- 
-@app.before_first_request
-#@app.route("/")
-#@app.route("/save_data_set_count_by_source")
 def save_data():
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_PATH))
+    template = env.get_template(TEMPLATE_NAME)
     csv_data = data_json_counts.update_csv_from_snapshots()
-    #return render_template('dataset_count_by_source.html',**locals())
+    rendered_html = template.render(csv_data=csv_data)
 
-    print( "render_template(dataset_count_by_source.html)...")
-    html_template = render_template('dataset_count_by_source.html',csv_data=csv_data)
-
-    #print( html_template)
-
-    print( "Save HTML file...")
-    with open("generated/data_json_counts.html", "w") as text_file:
-       text_file.write(html_template)
-
-    print( "shutdown_server()...")
-    shutdown_server()
- 
+    print( "Saving HTML file (size: "+str(len(rendered_html))+" bytes)..."+RENDERED_PATH+TEMPLATE_NAME)
+    with open(RENDERED_PATH+TEMPLATE_NAME, "w") as text_file:
+       text_file.write(rendered_html)
 
 
-@app.route("/")
-def home():
-    return "Data saved and shutting down"
-
- 
- 
 if __name__ == "__main__":
     print("running app.run...")
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    save_data()
