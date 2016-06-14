c18a49dbf653a0b498c5ddec25f892a4c1a3c027
David Portnoy <david@portnoy.us>
2016-06-14 15:23:59 -0500

    Added ability to find matching urls from data.json and categorize by agency

--- parse_ddod_content.py
+++ parse_ddod_content.py
@@ -111,6 +111,55 @@ def save_list_to_db(table_name,list_of_dicts):
 
 
 
+def load_data_json(file_name):
+    with open (file_name, "r") as myfile:
+        data_json=myfile.read()
+    return data_json
+
+
+
+def count_link_occurrences():
+    
+    #: Clean up encoding if needed
+    if data_json.count("\\/"):
+        data_json_clean = data_json.replace("\\/","/")
+    else:
+        data_json_clean = data_json
+        
+    for index,row in enumerate(ddod_smw_links):
+        url = ddod_smw_links[index]['extlinks'].strip("/")  # Trim trailing slash
+        url_count = data_json_clean.count(url)
+        ddod_smw_links[index]['in_hdgov'] = url_count
+
+
+        
+def extract_counts_by_agency(ddod_smw_links):
+    
+    counts_by_agency = {}
+    
+    for row in ddod_smw_links:
+        categories = row['categories']
+        in_hdgov   = row['in_hdgov']
+        
+        if ".com" in row['extlinks'].replace('.', '/.').split('/'):  # Ignore .com TLD
+            continue
+    
+        for category in categories.split(","):
+            if not category.count("-") and len(category)>0:     # Agency doesn't have a dash
+                agency = category
+                
+                if not agency in counts_by_agency:
+                    counts_by_agency[agency] = {}
+                    counts_by_agency[agency]['url_total'   ] = 0
+                    counts_by_agency[agency]['url_in_hdgov'] = 0
+
+                counts_by_agency[agency]['url_total'   ] += 1
+                counts_by_agency[agency]['url_in_hdgov'] += 1 if in_hdgov > 1 else 0
+            
+    return counts_by_agency
+            
+        
+
 rget_json = get_api_result(smw_url)
 ddod_smw_links = parse_smw_results(rget_json)
 
@@ -121,3 +170,6 @@ print("Loaded "+ str(len(ddod_smw_links)) +" records")
 # save_list_to_db("ddod_smw_links", ddod_smw_links)
 # df = save_list_to_df(ddod_smw_links)
 
+load_data_json("snapshots/HealthData.gov_2016-06-13_data.json")
+count_link_occurrences()
+counts_by_agency = extract_counts_by_agency(ddod_smw_links)        
