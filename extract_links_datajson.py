99dbde474393ddd2773468c1ad69e816162dc3b5
David Portnoy <david@portnoy.us>
2016-06-15 00:23:19 -0500

    Search through all sources.  Parse links from text.

--- extract_links_datajson.py
+++ extract_links_datajson.py
@@ -10,7 +10,10 @@ from data_json_tools import data_json_tools as tools
 import json
 import requests
 import dateutil.parser as dparser
-MAX_DAYS_OLD = 10
+import pandas as pd
+
+
+MAX_DAYS_OLD = 1
 
 
 
@@ -31,6 +34,60 @@ PREFIX_URL_LIST = [
      #,('HealthData.gov','http://healthdata.gov/data.json')
      #,('Data.gov' ,    'http://data.gov/data.json')
      ]
+
+
+
+import re
+REXEX_URL = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:[a-z\u00a1-\uffff]{2,})/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:[a-z\u00a1-\uffff]{2,})\b/?(?!@)))"""
+
+
+#=== Recursively parse through data.json looking for URLs
+def parse_json(source_name, source_obj, dest_str):
+    
+    if isinstance(source_obj, dict):
+        for key, value in source_obj.items():
+            parse_json(prefix, value, dest_str)
+    
+    if isinstance(source_obj, list):
+        for value in source_obj:
+            parse_json(prefix, value, dest_str)
+    
+    #: Strings may contain one or more URLs
+    if isinstance(source_obj, str):
+        target_str = source_obj.lower()
+        if "http" in target_str:
+            
+            #: Sometimes there are multiple URLs in the text
+            url_list = re.findall(REXEX_URL, target_str)
+            
+            if len(url_list) > 1 or (" " in target_str):
+                #print("INTERESTING TEXT ==> "+target_str)
+            
+            for url in url_list:
+                if url in dest_str:
+                    url_counts[source_name]["Found"].add(url)
+                    #print(url + " found")
+                else:
+                    url_counts[source_name]["NotFound"].add(url)
+                    #print(url + " not found")
+            
+    else:
+        return
+    
+
+
+
+
+def clean_up_dict(dirty_dict):
+
+    working_str = json.dumps(dirty_dict)
+    working_str = working_str.replace("\\/","/")
+    working_str = working_str.lower()
+    
+    clean_dict = json.loads(working_str)
+    return clean_dict
+
+
 
 
 def get_new_file_name(file_name_prefix, file_name_suffix='data.json'):
@@ -70,7 +127,7 @@ def get_datajson_from_web(url):
 
 def save_datajson_to_file(datajson_text, file_path):
     
-    with open(file_path, 'wb') as f:
+    with open(file_path, 'w') as f:
         f.write(datajson_text)
 
     return True  # Success
@@ -79,8 +136,8 @@ def save_datajson_to_file(datajson_text, file_path):
 
 def get_datajson_from_file(file_path):
     
-    with open(file_path, 'rb') as f:
-        datajson_text = f.read()
+    with open(file_path,encoding="ISO-8859-1") as f:
+        datajson_text = json.load(f)
 
     return datajson_text  # Success
 
@@ -102,25 +159,31 @@ def get_file_age(file_name):
 
 #: Determine whether to load from web or file
 
-def get_datajson(prefix, url):
+def get_datajson_dict(prefix, url):
     
     file_list = data_json_tools.get_file_list(max_load=1, file_date_pattern=['*']
                 , file_name_prefix=prefix
                 , file_name_suffix='[_]data.json'
                )
-    file_name = file_list[0]  # Latest only
-    file_age = get_file_age(file_name)
-    datajson_text = ""
+    if file_list:
+        file_name = file_list[0]  # Latest only
+        file_age = get_file_age(file_name)
+        print("Most recent file: " + file_name)
+    else:
+        file_name = False
+        file_age  = False
+        print("Not found:" + prefix)
 
     #=== If file is old or nonexistant, attempt to load from web 
-    if not file_date or file_age > MAX_DAYS_OLD:
+    if not file_name or file_age > MAX_DAYS_OLD:
 
         datajson_text = get_datajson_from_web(url)
 
         if datajson_text:
             new_file_name = get_new_file_name(prefix)
             save_datajson_to_file(datajson_text, new_file_name)
-            return datajson_text  # From web
+            datajson_dict = json.loads(datajson_text)
+            return datajson_dict  # From web
 
         
     #=== If no prior file and problem with web 
@@ -128,27 +191,50 @@ def get_datajson(prefix, url):
         return False
 
     #=== Otherwise use a file
-    datajson_text = get_datajson_from_file(file_name)
-    return datajson_text  # From file
+    datajson_dict = get_datajson_from_file(file_name)
+    return datajson_dict  # From file
     
-        
+
+    
+    
+    
 # Load HD.gov json
-hdgov_datajson_text = get_datajson('HealthData.gov','http://healthdata.gov/data.json')
+hdgov_datajson_dict = get_datajson_dict('HealthData.gov','http://healthdata.gov/data.json')
+hdgov_datajson_dict = clean_up_dict(hdgov_datajson_dict)
 
 
+
+#: Set one time values
+url_counts = {}
+dest_str    = json.dumps(hdgov_datajson_dict)
+
+
+#: Loop through sources
 # Load source json files
 for (prefix,url) in PREFIX_URL_LIST:
-    datajson_text = get_datajson(prefix, url)
-    print(len(datajson_text))
+    datajson_dict = get_datajson_dict(prefix, url)
+    print(len(datajson_dict))
     
-    ##
-    ##  Parse text for list of URLs
-    ##
+    source_name = prefix
+    url_counts[source_name] = {}
+    url_counts[source_name]['Found']    = set()   # Use set to avoid duplicates
+    url_counts[source_name]['NotFound'] = set()   # Use set to avoid duplicates
+    parse_json(prefix, datajson_dict, dest_str)
     
-    ##
-    ##  Look for each URL in target (HD.gov)
-    ##
+
+
+#=== Format results 
+url_results = []
+for key, value in url_counts.items():
+    data_source    = key
+    search_results = value
+    num_found     = len(search_results['Found'])
+    num_not_found = len(search_results['NotFound'])
+    url_results.append({ "Data Source": data_source
+                        ,"Found":       num_found
+                        ,"Not Found":   num_not_found
+                       })
     
-    ##
-    ##  Document counts
-    ##
+df = pd.DataFrame(data=url_results)
+print(df)
+    
